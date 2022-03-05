c048601a288b89a5ffa38af194b5d0cf979f9be6
Ali Yıldız <yildizaalii@gmail.com>
2022-03-05 14:40:54 +0300

    Initial Commit

---
+++ FavoriteBookSwiftUI/FavoriteBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookSwiftUIApp.swift
+//  FavoriteBookSwiftUI
+//
+//  Created by Ali on 5.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
